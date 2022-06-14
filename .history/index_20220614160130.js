3bb20eb72e371e9e6960aec75e90a4c62153940a
peterclermy <peterclermy232@gmail.com>
2022-06-14 16:07:49 +0300

    commit tree cf14f8e58e1a915b1980ad716cf0db5c68e34e38

--- .history/index_20220614160130.js
+++ .history/index_20220614160130.js
@@ -1,8 +1,6 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  return fetch("https://anapioficeandfire.com/api/books")
-    .then(res => res.json())
-    .then(json => renderBooks(json));
+  fetch("https://anapioficeandfire.com/api/books");
 }
 
 function renderBooks(books) {
